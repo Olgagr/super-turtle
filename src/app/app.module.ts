aaaf02b33e42cf5c3ba6e1497eb3151143917ecb
Olga Grabek <olga.grabek@twipemobile.com>
2021-07-30 13:29:42 +0200

    Remove taiga

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TuiRootModule } from '@taiga-ui/core';
 import { AppComponent } from './app.component';
 import { CanvasComponent } from './canvas/canvas.component';
 import { EditorComponent } from './editor/editor.component';
@@ -17,12 +16,7 @@ import { ToolBarComponent } from './tool-bar/tool-bar.component';
     CanvasComponent,
     ToolBarComponent,
   ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    TuiRootModule,
-    BrowserAnimationsModule,
-  ],
+  imports: [BrowserModule, ReactiveFormsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
